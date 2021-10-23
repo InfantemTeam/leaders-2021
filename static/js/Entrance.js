6cb739fddcab6814a78659efc32e48e08205f635
Egor Vorontsov <sdoregor@sdore.me>
2021-10-24 02:12:48 +0300

    temp

--- static/js/Entrance.js
+++ static/js/Entrance.js
@@ -1,7 +1,7 @@
 function checkParams() {
     var login = $('#login').val();
     var password = $('#password').val();
-     
+
     if(login.length != 0 && password.length != 0) {
         $('#submit').removeAttr('disabled');
     } else {
