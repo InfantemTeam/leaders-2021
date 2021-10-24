0ca144aee863208fa6c0d80eebde61794b62aabe
Karim90403 <karim90403@gmail.com>
2021-10-24 19:34:06 +0300

    Фронт закончен

--- static/js/selector.js
+++ static/js/selector.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-	$(".selector").chosen()
-});
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
