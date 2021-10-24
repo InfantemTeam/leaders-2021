8ac45c8b97329691f40d5a8ab08f53b486e9aee7
Egor Vorontsov <sdoregor@sdore.me>
2021-10-24 07:02:26 +0300

    base to main

--- test.py
+++ test.py
@@ -16,10 +16,6 @@ async def login():
 async def lk():
 	return await render_template('lk.html')
 
-@app.route('/profile/edit')
-async def profile_edit():
-	return await render_template('profile_edit.html')
-
 if (__name__ == '__main__'):
 	app.run('0.0.0.0', debug=True)
 
